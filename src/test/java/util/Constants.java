075c21225ea33414f1e5db52f80385418e89275f
vishmridul <vishmridul@gmail.com>
2021-05-28 05:01:30 +0530

    Signed-off-by: vishmridul <vishmridul@gmail.com>

--- src/test/java/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public final class Constants {
-
-  public static final String URL="https://blazedemo.com/";
-  public static final String SPACEX_URL_END_POINT="https://api.spacexdata.com/v4/launches/latest";
-
-
-}
